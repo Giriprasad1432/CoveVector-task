2322481d7780f497275d5ffe49f443382801b7e3
Giri prasad <giribabu121989@gmail.com>
2026-06-24 21:47:51 +0530

    seed script changes

--- backend/seed.js
+++ backend/seed.js
@@ -1,24 +1,32 @@
 import mongoose from "mongoose";
-import connectDB from "./config/db";
-import product from "./models/productModel";
+import connectDB from "./config/db.js";
+import product from "./models/productModel.js";
 
-connectDB();
+await connectDB();
 
-const seed = async() => {
-    const batch=200;
-    const categories=['Electronics','books','toys','Fashion','Cookware','Grocery']
-    for(let i=1; i<=batch ;i++){
-        const products=[];
-        for(let j=1;j<=1000;j++){
-            const id=i*1000+j;
-            products.push({
-                productId:`PROD-${String(id).padStart(6,'0')}`,
-                name:`Product-${batch*id}`,
-                category: categories[Math.floor(Math.random()*categories.length)],
-                price:Math.random()*1000+id*50
-            })
+const seed = async () => {
+    try {
+        console.log('seeding database...')
+        const batch = 200;
+        const categories = ['Electronics', 'books', 'toys', 'Fashion', 'Cookware', 'Grocery']
+        for (let i = 0; i < batch; i++) {
+            const products = [];
+            for (let j = 1; j <= 1000; j++) {
+                const id = i * 1000 + j;
+                products.push({
+                    productId: `PROD-${String(id).padStart(6, '0')}`,
+                    name: `Product-${id}`,
+                    category: categories[Math.floor(Math.random() * categories.length)],
+                    price: parseFloat((Math.random() * 1000 + id * 0.05).toFixed(2))
+                })
+            }
+            await product.insertMany(products,{ordered:false ,lean:true});
         }
-        await product.insertMany(products);
+    }catch(err){
+        console.log(err)
+    }finally{
+        await mongoose.connection.close();
+        console.log("Database connection closed.");
     }
 
 }
